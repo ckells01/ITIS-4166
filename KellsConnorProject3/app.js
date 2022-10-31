b92e9ef7a0ee081834a137510da3f1fa0973baa2
ckells01 <ckells@uncc.edu>
2022-10-31 16:26:52 -0400

    add project3

---
+++ KellsConnorProject3/app.js
@@ -0,0 +1,54 @@
+// Require modules
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const eventRoutes = require('./routes/eventRoutes');
+const mainRoutes = require('./routes/mainRoutes');
+
+// Create app
+const app = express();
+
+// Configure app
+let port = 8084;
+let host = 'localhost';
+app.set('view engine', 'ejs');
+
+// Mount middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
+
+// Setup routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/event', (req, res) => {
+    res.render('events');
+});
+
+app.use('/events', eventRoutes);
+app.use('/', mainRoutes);
+
+// Error handling
+app.use((req, res, next) => {
+    let err = new Error('The server cannot locate ' + req.url);
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    if (!err.status) {
+        err.status = 500;
+        err.message = ("Internal Server Error");
+    }
+    res.status(err.status);
+    res.render('error', {error: err});
+});
+
+// Start server
+app.listen(port, host, () => {
+    console.log('Server is running on port', port);
+});
---
+++ KellsConnorProject3/models/event.js
@@ -0,0 +1,109 @@
+const { dateTime, DateTime } = require("luxon");
+const {v4: uuidv4} = require('uuid');
+
+const events = [
+{
+    id: '1',
+    title: 'Halo: HCS Majors Livestream',
+    host: 'HCS',
+    category: 'Livestream',
+    location: 'HCS Twitch Channel',
+    date: '2022-10-25',
+    startTime: '18:30',
+    endTime: '21:00',
+    description: 'The Halo Championship Major livestream'
+},
+{
+    id: '2',
+    title: 'Apex Legends: Team Qualifiers Livestream',
+    host: 'EA eSports',
+    category: 'livestream',
+    location: 'Apex Twitch Channel',
+    date: '2022-10-25',
+    startTime: '10:45',
+    endTime: '15:00',
+    description: 'The Apex Legends team qualifiers livestream for the upcoming competitive season.'
+},
+{
+    id: '3',
+    title: 'Call of Duty: CDL Major 1 Livestream',
+    host: 'COD World League',
+    category: 'stream',
+    location: 'CDL Twitch Channel',
+    date: '2022-12-5',
+    startTime: '12:30',
+    endTime: '15:30',
+    description: 'The Call of Duty competetive league Major 1 Livestream.'
+},
+{
+    id: '4',
+    title: 'Halo: 4v4 Custom Party Games',
+    host: 'Niner Esports',
+    category: 'group play',
+    location: 'Student Union, Room 310',
+    date: '2022-11-2',
+    startTime: '11:00',
+    endTime: '13:00',
+    description: 'Come meet other Halo fans and play fun customized game modes (8 people needed!)',
+},
+{
+    id: '5',
+    title: 'Apex Legends: Trios Ranked Play',
+    host: 'Niner Esports',
+    category: 'group Play',
+    location: 'Xbox Live',
+    date: '2022-10-25',
+    startTime: '09:30',
+    endTime: '12:30',
+    description: 'Play with other ranked Apex Legends players as a trio to rank up in competitive online play (Three players needed).',
+},
+{
+    id: '6',
+    title: 'Call of Duty: Search and Destroy Tournament',
+    host: 'Niner Esports',
+    category: 'play',
+    location: 'Student Union, Room 200',
+    date: '2022-10-25',
+    startTime: '15:00',
+    endTime: '17:00',
+    description: 'Play in an in-person Call of Duty: Search and Destroy tournament (6v6) and meet other CoD fans (12 players needed).',
+}
+];
+
+exports.find = () => events;
+
+exports.findById = function(id) {
+    return events.find(event => event.id === id);
+}
+
+exports.save = function(event) {
+    event.id = uuidv4();
+    events.push(event);
+}
+
+exports.updateById = function(id, newevent) {
+    let event = events.find(event => event.id === id);
+    if(event) {
+        event.title = newevent.title;
+        event.host = newevent.host;
+        event.category = newevent.category;
+        event.location = newevent.location;
+        event.date = newevent.date;
+        event.startTime = newevent.startTime;
+        event.endTime = newevent.endTime;
+        event.description = newevent.description;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+exports.deleteById = function(id) {
+    let index = events.findIndex(event => event.id === id);
+    if (index !== -1) {
+        events.splice(index, 1);
+        return true;
+    } else {
+        return false;
+    }
+}
