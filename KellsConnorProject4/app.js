fead6153caed036986906a89d0bba27cf9355f9a
ckells01 <ckells@uncc.edu>
2022-12-03 03:12:33 -0500

    project4 first updates runs

--- KellsConnorProject4/app.js
+++ KellsConnorProject4/app.js
@@ -5,6 +5,11 @@ const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const eventRoutes = require('./routes/eventRoutes');
 const mainRoutes = require('./routes/mainRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
 
 // Create app
 const app = express();
@@ -12,20 +17,38 @@ const app = express();
 // Configure app
 let port = 8084;
 let host = 'localhost';
-let url = 'mongodb://localhost:27017/NBAD'
 app.set('view engine', 'ejs');
 
-// Connect to MongoDB
-mongoose.connect(url)
-.then(() => {
+//connect to database
+mongoose.connect('mongodb://localhost:27017/NBAD', 
+                {useNewUrlParser: true, useUnifiedTopology: true, /**useCreateIndex: true**/ })
+.then(()=>{
     // Start the server
     app.listen(port, host, () => {
         console.log('Server is running on port', port);
-    })
+    });
 })
-.catch(err => console.log(err.message));
+.catch(err=>console.log(err.message));
 
 // Mount middleware
+app.use(
+    session({
+        secret: "ajfeirf90aeu9eroejfoefj",
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({mongoUrl: 'mongodb://localhost:27017/NBAD'}),
+        cookie: {maxAge: 60*60*1000}
+        })
+);
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.user = req.session.user||null;
+    res.locals.errorMessages = req.flash('error');
+    res.locals.successMessages = req.flash('success');
+    next();
+});
+
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
@@ -42,6 +65,7 @@ app.use(methodOverride('_method'));
 
 app.use('/events', eventRoutes);
 app.use('/', mainRoutes);
+app.use('/users', userRoutes);
 
 // Error handling
 app.use((req, res, next) => {
@@ -59,3 +83,4 @@ app.use((err, req, res, next) => {
     res.status(err.status);
     res.render('error', {error: err});
 });
+
